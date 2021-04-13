1e955fe68b9cdb9a627a1691a779e40e36d9bc50
MonkeyCode7 <79100835+MonkeyCode7@users.noreply.github.com>
2021-04-13 13:45:18 +0300

    Add files via upload

--- Koodia/2303/viikko3ToisetHarjoitukset.js
+++ Koodia/2303/viikko3ToisetHarjoitukset.js
@@ -135,23 +135,5 @@ function arvoKortit()
       temp = pakka[Math.floor(Math.random()*52)];
       kortit[l] = temp;
   }
-  document.getElementById('pakka').innerHTML = '<p>'+kortit+'</p>';
-}
-//Tehtävä 5//
-function teeLomake()
-{
-  var lomake = '<p>Anna tilinro:<input type="text" id="tilinro" value=""></p>';
-  var lomake = lomake + '<p>Anna etunimi:<input type="text" id="enimi" value=""></p>';
-  var lomake = lomake + '<p>Anna sukunimi:<input type="text" id="snimi" value=""></p>';
-  var lomake = lomake + '<p>Anna saldo:<input type="text" id="saldo" value=""></p>';
-  var lomake = lomake + '<p>Haluatko jatkaa (k/e):<input type="text" id="lopetus" value=""></p>';
-  document.getElementById('lomake').innerHTML = lomake;
-  laskuri = 0;
-  lopetus = 'e';
-  do
-  {
-
-    lopetus = document.getElementById('lopetus').value;
-  }while(lopetus = 'k');
-
+  document.getElementById('pakka').innerHTML = '<p> ' +kortit+ ' </p>';
 }
